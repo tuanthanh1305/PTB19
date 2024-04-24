cb3c8bafe97e7946ee7bdf7d70f3c3b8856b7efc
tuanthanh1305 <130335648+tuanthanh1305@users.noreply.github.com>
2024-04-24 20:47:50 +0700

    update

--- bai2ptb.py
+++ bai2ptb.py
@@ -1,2 +1,17 @@
-x, y, z = 10, 5, 6
-print(x, y, z)
+# x, y, z = 10, 5, 6
+# print(x, y, z)
+
+#vi du ve kieu du lieu
+
+# name = "Tran Tuan Thanh"
+# age = 2024-1900
+# gpa = 9.8
+
+# print( name, age, gpa)
+
+#Vi du ve cau lenh nhan biet kieu du lieu cua bien
+# bienint = 20
+# print(type(bienint))
+
+
+
